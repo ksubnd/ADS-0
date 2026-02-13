824abd0b527ea29291a66ab80a45f6c7d18c5ed3
ksubnd <ksubnd@gmail.com>
2026-02-13 23:04:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,20 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-	return 25;
+	if (b > a)
+	{
+		int temp = 0;
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	if (a % b == 0)
+	{
+		return b;
+	}
+	for (a; a % b != 0; a = b)
+	{
+		b = a % b;
+	}
+	return b;
 }
